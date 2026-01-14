a3072f22c9c080d5eff8faa673510ab99518acb5
thumb0907 <lthsty79@gmail.com>
2026-01-14 18:16:00 +0900

    로봇팔

--- Automated-Aquaponics-System/sec_convey/Omx_Manual/Omx_Manual.ino
+++ Automated-Aquaponics-System/sec_convey/Omx_Manual/Omx_Manual.ino
@@ -3,26 +3,26 @@
 
 void setup()
 {
+  
   Serial.begin(115200);
-  step_pin();
-  delay(1000);
   initManipulator();
+  pinMode(PUL, OUTPUT);
+  pinMode(DIR, OUTPUT);
+
+  digitalWrite(PUL, HIGH); // 기본 OFF (옵토 끔)
+
   Serial.println("==== OpenManipulator Started! ====");
 }
 
 
 void loop()
 {
-  //moveHome();
-  static float pps = 300;
-  static int dir = +1;
-
-  pps += dir * 50;          // 50ms마다 50pps씩 변화
-  if (pps > 3000) { pps = 3000; dir = -1; }
-  if (pps < 300)  { pps = 300;  dir = +1; }
-
-  step_setStepsPerSec(pps);
-  delay(50);
+  moveHome();
+  
+  for (int i = 0; i < 2000; i++) stepPulse(dir_state, 100);
+  
+  dir_state = !dir_state;      // 다음 루프에서 방향 반대
+  delay(1000);
 }
 
 
